3a92116eddcc1cd0de3a8374dbd98422d643f855
Dennis Risen <dennis.risen@case.edu>
2022-12-23 23:12:43 -0600

    refactored credentials into mylib

--- src/mylib/__init__.py
+++
@@ -1,5 +0,0 @@
-from mylib import anyToSecs, credentials, fromTimeStamp, home_zone, logErr, \
-    millisToSecs, printIf, secsToMillis, strfTime, strpSecs, strpTime, verbose_1
-__all__ = ['credentials', 'anyToSecs', 'fromTimeStamp', 'home_zone', 'logErr',
-           'millisToSecs', 'printIf', 'secsToMillis', 'strfTime', 'strpSecs',
-           'strpTime', 'verbose_1']
---
+++ src/mylib/__init__.txt
@@ -0,0 +1,5 @@
+from mylib import anyToSecs, credentials, fromTimeStamp, home_zone, logErr, \
+    millisToSecs, printIf, secsToMillis, strfTime, strpSecs, strpTime, verbose_1
+__all__ = ['credentials', 'anyToSecs', 'fromTimeStamp', 'home_zone', 'logErr',
+           'millisToSecs', 'printIf', 'secsToMillis', 'strfTime', 'strpSecs',
+           'strpTime', 'verbose_1']
